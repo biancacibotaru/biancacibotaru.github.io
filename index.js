40a3cb9a7ed47994308798ab4acb0ab3df42f107
Bianca Cibotaru <bianca.cibotaru@indsoft.ro>
2023-06-15 18:25:29 +0300

    Add activePage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var activePage = "home";
+
 function hide(id) {
     document.getElementById(id).style.display = "none";
 }
@@ -7,16 +9,10 @@ function show(id) {
     page.style.display = "block";
 }
 
-function hideAllPages() {
-    hide("home");
-    hide("skills");
-    hide("languages");
-    hide("projects");
-}
-
 function displayPage(id) {
-    hideAllPages();
+    hide(activePage);
     show(id);
+    activePage = id;
 }
 
-displayPage("home");
+displayPage(activePage);
