9fde225faa58404dbb18ef499a5f606803539a0a
Bianca Cibotaru <bianca.cibotaru@indsoft.ro>
2023-06-15 20:34:36 +0300

    Format code with prettier

--- examples.js
+++ examples.js
@@ -9,13 +9,13 @@ console.info(typeof name);
 employed = `yes working @ "RWS"!, I'll still work there.`;
 console.info(typeof employed, employed);
 
-var skills = [1, 'html', `css`, true]
+var skills = [1, "html", `css`, true];
 console.info(typeof skills, skills[1], skills.length);
 
 var person = {
-    name: "Bianca",
-    age: 22,
-    employed: true,
-    skills: ["html", "css", "js"],
+  name: "Bianca",
+  age: 22,
+  employed: true,
+  skills: ["html", "css", "js"],
 };
 console.warn(typeof person, person);
