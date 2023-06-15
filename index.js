b5ceac9a1f06406c27428ce71a68d8c3b56f38c6
Bianca Cibotaru <bianca.cibotaru@indsoft.ro>
2023-06-15 20:29:43 +0300

    Format js

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 var activePage = "skills";
 
 function hide(id) {
-    $("#" + id).style.display = "none";
+  $("#" + id).style.display = "none";
 }
 
 function show(id) {
-    var page = $(`#${id}`)
-    page.style.display = "block";
+  var page = $(`#${id}`);
+  page.style.display = "block";
 }
 
 function $(selector) {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
 }
 
 function displayPage(id) {
-    hide(activePage);
-    $(`#top-menu-bar a[data-page="${activePage}"]`).classList.remove("active")
-    activePage = id;
-    show(id);
-    $(`#top-menu-bar a[data-page="${id}"]`).classList.add("active");
+  hide(activePage);
+  $(`#top-menu-bar a[data-page="${activePage}"]`).classList.remove("active");
+  activePage = id;
+  show(id);
+  $(`#top-menu-bar a[data-page="${id}"]`).classList.add("active");
 }
 
 function clickOnMenu(e) {
-    if (e.target.matches("a")) {
-        var id = e.target.dataset.page;
-        if (id) {
-           displayPage(id);
-        } else {
-          console.warn('please use <a data-page="pageid"> ');
-        }
+  if (e.target.matches("a")) {
+    var id = e.target.dataset.page;
+    if (id) {
+      displayPage(id);
+    } else {
+      console.warn('please use <a data-page="pageid"> ');
     }
+  }
 }
 
 function showSkillsList() {
-    var skills = [
-        {name: "HTML", favorite: true },
-        {name: "CSS" },
-        {name: "JS", favorite: true }
-    ];
+  var skills = [
+    { name: "HTML", favorite: true },
+    { name: "CSS" },
+    { name: "JS", favorite: true },
+  ];
 
-    var ul = $("#skills ul");
-    var skillsHTML = skills.map(function(skill){
-        var className = skill.favorite? "favorite" : "";
-        return `<li class="${className}">${skill.name}</li>`;
-    });
-    ul.innerHTML = skillsHTML.join("");
-    console.info(skillsHTML);
+  var ul = $("#skills ul");
+  var skillsHTML = skills.map(function (skill) {
+    var className = skill.favorite ? "favorite" : "";
+    return `<li class="${className}">${skill.name}</li>`;
+  });
+  ul.innerHTML = skillsHTML.join("");
+  console.info(skillsHTML);
 }
 
 displayPage(activePage);
